50632ca7477258fc572268b14106417c86f3a419
Andrey Sergienia <andrey.sergienia@gmail.com>
2015-06-07 20:05:53 +0300

    Fix: add reject for empties questions

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,6 +1,8 @@
 class Vote < ActiveRecord::Base
   has_many :questions, :dependent => :destroy
-  accepts_nested_attributes_for :questions, :reject_if => :all_blank, :allow_destroy => true
+  accepts_nested_attributes_for :questions,
+                                :reject_if => proc { |a| a[:answers_attributes].nil? },
+                                :allow_destroy => true
   belongs_to :user
   validates :name, presence: true
 end
