55f2b7ba8d6c1e27f70457d3321c86d4cfcd5a64
Andrey Sergienia <andrey.sergienia@gmail.com>
2015-06-07 16:47:23 +0300

    New: add question model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150607112810) do
+ActiveRecord::Schema.define(version: 20150607134633) do
+
+  create_table "questions", force: :cascade do |t|
+    t.text     "text"
+    t.integer  "vote_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "questions", ["vote_id"], name: "index_questions_on_vote_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
