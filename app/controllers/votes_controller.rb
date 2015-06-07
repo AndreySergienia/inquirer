73b7a8a77d3efcb4b97a278b00ad7db7e5c48896
Andrey Sergienia <andrey.sergienia@gmail.com>
2015-06-07 15:28:59 +0300

    New: add creating vote

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -6,4 +6,23 @@ class VotesController < ApplicationController
   def show
     @vote = Vote.find_by(id: params[:id])
   end
+
+  def new
+    @vote = Vote.new
+  end
+
+  def create
+    @vote = Vote.new(vote_params)
+    if @vote.save
+      flash[:success] = "Vote successfully created!"
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def vote_params
+      params.require(:vote).permit(:name)
+    end
 end
