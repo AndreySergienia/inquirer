5f5e2f570350ad1cfd8b7a12cf1e8b431c99f7f4
Andrey Sergienia <andrey.sergienia@gmail.com>
2015-06-07 14:49:41 +0300

    New: add show for vote

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,4 +2,8 @@ class VotesController < ApplicationController
   def index
     @votes = Vote.all
   end
+
+  def show
+    @vote = Vote.find_by(id: params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'votes#index'
+  resources :votes
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
