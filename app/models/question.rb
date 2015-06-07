8b39248018e1a43aee96a7d09fcc0a709ee19028
Andrey Sergienia <andrey.sergienia@gmail.com>
2015-06-07 18:08:22 +0300

    New: add model answer

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,6 @@
 class Question < ActiveRecord::Base
+  has_many :answers, dependent: :destroy
+  accepts_nested_attributes_for :answers, :reject_if => :all_blank, :allow_destroy => true, :limit => 4
   belongs_to :vote
   validates :text, presence: true
 end
