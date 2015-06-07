8479173755126a57aaf239308703e0c6151089fc
Andrey Sergienia <andrey.sergienia@gmail.com>
2015-06-07 16:00:32 +0300

    New: add edit vote

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -21,6 +21,30 @@ class VotesController < ApplicationController
     end
   end
 
+  def destroy
+    @vote = Vote.find_by(id: params[:id])
+    if @vote.destroy
+      flash[:success] = "Vote successfully deleted!"
+    else
+      flash[:error] = "Error!"
+    end
+    redirect_to root_path
+  end
+
+  def edit
+    @vote = Vote.find_by(id: params[:id])
+  end
+
+  def update
+    @vote = Vote.find_by(id: params[:id])
+    if @vote.update(vote_params)
+      flash[:success] = "Vote successfully updated"
+      redirect_to root_path
+    else
+      render 'edit'
+    end
+  end
+
   private
     def vote_params
       params.require(:vote).permit(:name)
