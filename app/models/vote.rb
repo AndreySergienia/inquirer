596d633203e3408fc8ac4b06df6fec0012a4e8dc
Andrey Sergienia <andrey.sergienia@gmail.com>
2015-06-07 19:06:19 +0300

    New: add user to vote

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,5 +1,6 @@
 class Vote < ActiveRecord::Base
   has_many :questions, :dependent => :destroy
   accepts_nested_attributes_for :questions, :reject_if => :all_blank, :allow_destroy => true
+  belongs_to :user
   validates :name, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150607145301) do
+ActiveRecord::Schema.define(version: 20150607160023) do
 
   create_table "answers", force: :cascade do |t|
     t.text     "text"
@@ -54,6 +54,9 @@ ActiveRecord::Schema.define(version: 20150607145301) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "votes", ["user_id"], name: "index_votes_on_user_id"
 
 end
